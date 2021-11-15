456a39e7c8f8fd2160a6b21811e8b45232dab986
egreich <egr65@nau.edu>
2021-11-15 11:13:59 -0700

    Fixed pull request changes

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -1,7 +1,6 @@
 ### This script will input the combined pulse meta-analysis extraction csv files
-### and create figures that summarize the collection
-
-setwd("~/Documents/Emma/NAU/Pulse_Meta") # set to your working directory
+### create figures that summarize the collection
+### and select variables for prelim analysis
 
 
 library(tidyverse)
@@ -12,9 +11,9 @@ library(lubridate)
 
 
 # Import data
-dataIN_study = read.csv("Combined_study-source_info.csv", header = TRUE, skip = 1)
-dataIN_pulse = read.csv("Combined_pulse-plot_info.csv", header = TRUE, skip = 1)
-dataIN_record = read.csv("Combined_record_info.csv", header = TRUE, skip = 1)
+dataIN_study = read.csv("./data_raw/Combined_study-source_info.csv", header = TRUE, skip = 1)
+dataIN_pulse = read.csv("./data_raw/Combined_pulse-plot_info.csv", header = TRUE, skip = 1)
+dataIN_record = read.csv("./data_raw/Combined_record_info.csv", header = TRUE, skip = 1)
 
 
 # Make paper column for counting
@@ -23,7 +22,7 @@ d_study <- dataIN_study %>% mutate(paper = substr(Study.ID,1L,3L)) # extract fir
 total_papers <- unique(d_study$paper)
 length(total_papers)
 
-# Make vegetation count graph
+### Make vegetation count graph
 
 d_veg_count <- d_study %>%
   count(Vegetation.type)
@@ -31,6 +30,8 @@ d_veg_count <- d_veg_count[-1,]
 d_veg_count <- d_veg_count %>%
   mutate()
 
+# The parentheses here just automatically call the plot
+  # similar to just typing p_veg after
 (p_veg <- ggplot(d_veg_count) +
   geom_bar(aes(x=n, y=Vegetation.type), position="dodge", stat = "identity") +
   labs(title = NULL, y = NULL, x = "count") +
@@ -43,9 +44,10 @@ d_veg_count <- d_veg_count %>%
         axis.text.x = element_text(colour="black", angle = 90),
         plot.title = element_text(hjust = 0.5)))
 
-path_out = "~/Documents/Emma/NAU/Pulse_Meta/graphs/" # set save path
+if(!file.exists("plots")) { dir.create("plots")} # create plots folder if it doesn't exist
+path_out = "./plots/" # set save path
 
-ggsave2("p_veg.png", plot = p_veg, path = path_out)
+ggsave2("p_veg.png", plot = p_veg, path = path_out) # save veg type count plot
 
 ### Count natural and experimental pulses
 
@@ -71,8 +73,6 @@ d_var_count <- dataIN_record %>%
           axis.line = element_line(color = "black"),
           axis.text.x = element_text(colour="black", angle = 90),
           plot.title = element_text(hjust = 0.5)))
-
-path_out = "~/Documents/Emma/NAU/Pulse_Meta/graphs/" # set save path
 
 ggsave2("p_var.png", plot = p_var, path = path_out)
 
@@ -105,15 +105,12 @@ ggsave2("p_var.png", plot = p_var, path = path_out)
 ### (see 11/3 /21 meeting notes)
 
 # Drop rows with variables we don't care about right now
-d_record <-subset(dataIN_record, 
-                  Variable!="RH" & Variable!="VPD" & Variable!="Air temp" & Variable!="observed leaf delta18O"
-                  & Variable!="observed evaporation delta18O" & Variable!="observed ET delta18O" & Variable!="modelled T delta18O"
-                  & Variable!="modelled evaporation delta18O" & Variable!="observed leaf delta18O" & Variable!="Euclidean distance from orgin"
-                  & Variable!="Euclidean distance" & Variable!="Cumulative euclidean distance" & Variable!="biomass increment"
-                  & Variable!="ci" & Variable!="modelled evaporation")
-
 # Combine variable names in a new row
-d_record <- d_record %>%
+# If the newVariable column has NAs, fill in with Variable column
+d_record <- dataIN_record %>%
+  filter(!Variable %in% c("RH", "VPD", "Air temp", "observed leaf delta18O", "observed evaporation delta18O", "observed ET delta18O",
+                          "modelled T delta18O", "modelled evaporation delta18O", "observed leaf delta18O", "Euclidean distance from orgin",
+                          "Euclidean distance", "Cumulative euclidean distance", "biomass increment", "ci", "modelled evaporation")) %>%
   mutate(newVariable = case_when(Variable %in% c("rsoil", "rhetero", "rauto", "LN(rsoil)") ~ "belowgroundR",
                                  Variable %in% c("rd") ~ "abovegroundgroundR",
                                  Variable %in% c("reco") ~ "ecosystemR",
@@ -122,20 +119,19 @@ d_record <- d_record %>%
                                  Variable %in% c("gs", "stomatal conductance") ~ "stomatal conductance",
                                  Variable %in% c("VWC", "soil water content") ~ "soil water content",
                                  Variable %in% c("stem sapflow velocity", "sap velocity", "root sap velocity" , 
-                                                 "lateral root sapflow velocity", "primary sinker root sapflow velocity", "t (transpiration)") ~ "T"))
-        # should we also combine anet and PAR?
-
-# If the newVariable column has NAs, fill in with Variable column
-d_record$newVariable <- ifelse(is.na(d_record$newVariable), d_record$Variable, d_record$newVariable )
-
+                                                 "lateral root sapflow velocity", "primary sinker root sapflow velocity", "t (transpiration)") ~ "T"),
+         newVariable = ifelse(is.na(newVariable), Variable, newVariable))
+        
+  # should we also combine anet and PAR?
 
 (var_names <- unique(d_record$newVariable)) # check the new variable names
 
-# Make a new variable count plot
+### Make a new variable count plot
 d_var_count <- d_record %>%
   count(newVariable)
 
-(p_var <- ggplot(d_var_count) +
+
+(p_var_filtered <- ggplot(d_var_count) +
     geom_bar(aes(x=n, y=newVariable), position="dodge", stat = "identity") +
     labs(title = NULL, y = "variables", x = "measurements") +
     theme(legend.position = "right",
@@ -146,6 +142,8 @@ d_var_count <- d_record %>%
           axis.line = element_line(color = "black"),
           axis.text.x = element_text(colour="black", angle = 90),
           plot.title = element_text(hjust = 0.5)))
+
+ggsave2("p_var_filtered.png", plot = p_var_filtered, path = path_out)
 
 
 
