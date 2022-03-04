c0ffe8ef9bc293b8fc28ccf01515b82cee3c8eb8
Jessica Guo <jessicaguo@email.arizona.edu>
2022-03-04 13:35:24 -0700

    Add a second table with pulse overlaps included

--- scripts/03_populate_matrix.R
+++ scripts/03_populate_matrix.R
@@ -30,14 +30,18 @@ diag <- d_clean  %>%
             n_study = length(unique(Study.ID))) %>%
   filter(varType %in% variables) 
 
-count_mat <- matrix(NA, 
+count_mat1 <- matrix(NA, 
                     nrow = length(variables),
                     ncol = length(variables))
+count_mat2 <- matrix(NA, 
+                     nrow = length(variables),
+                     ncol = length(variables))
 
 for(i in 1:length(variables)) {
   for(j in 1:length(variables)) {
      if(i == j) { # diagonal
-       count_mat[i, j] <- diag$n_record[i]
+       count_mat1[i, j] <- diag$n_record[i]
+       count_mat2[i, j] <- diag$n_pulse[i]
      } else if(i > j){ # lower left half
         o_study <- d_clean %>%
          filter(varType == variables[i] | varType == variables[j]) %>%
@@ -45,14 +49,17 @@ for(i in 1:length(variables)) {
          summarize(nvar = length(unique(varType))) %>%
          filter(nvar == 2) %>%
          nrow()
-        count_mat[i, j] <- o_study
-        # o_pulse <- d_clean %>%
-        #   filter(varType == variables[i] | varType == variables[j]) %>%
-        #   group_by(study_pulse) %>%
-        #   summarize(nvar = length(unique(varType))) %>%
-        #   filter(nvar == 2) %>%
-        #   nrow() 
-
+        
+        o_pulse <- d_clean %>%
+          filter(varType == variables[i] | varType == variables[j]) %>%
+          group_by(study_pulse) %>%
+          summarize(nvar = length(unique(varType))) %>%
+          filter(nvar == 2) %>%
+          nrow()
+        
+        count_mat1[i, j] <- o_study
+        count_mat2[i, j] <- o_pulse
+        
      } else if(j > i) { # upper right half
        o_record <- d_clean %>%
          filter(varType == variables[i] | varType == variables[j]) %>%
@@ -60,21 +67,29 @@ for(i in 1:length(variables)) {
          summarize(nvar = length(unique(varType))) %>%
          filter(nvar == 2) %>%
          nrow() 
-       count_mat[i, j] <- o_record
+       
+       count_mat1[i, j] <- o_record
+       count_mat2[i, j] <- o_record
      }
   }
 }
 
 # Convert to dataframe, label
-count_df <- data.frame(count_mat) 
-colnames(count_df) <- variables
-rownames(count_df) <- variables
+count1_df <- data.frame(count_mat1) 
+colnames(count1_df) <- variables
+rownames(count1_df) <- variables
+
+count2_df <- data.frame(count_mat2) 
+colnames(count2_df) <- variables
+rownames(count2_df) <- variables
 
 # write out
 if(!dir.exists("data_clean/tables")) {
   dir.create("data_clean/tables")
 }
-write.csv(count_df, file = "data_clean/tables/matrix_study_record.csv")
+write.csv(count1_df, file = "data_clean/tables/matrix_study_record.csv")
+
+write.csv(count2_df, file = "data_clean/tables/matrix_pulse_record.csv")
 
 
 
