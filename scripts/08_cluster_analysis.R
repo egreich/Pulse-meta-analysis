b00b6f1d9dfa5e7c7ad7950edbc5ef74e0d3e264
egreich <egr65@nau.edu>
2023-05-22 11:40:28 -0700

    add cluster analysis notes

--- scripts/08_cluster_analysis.R
+++ scripts/08_cluster_analysis.R
@@ -1,1 +1,21 @@
-### 
+### Post-analysis cluster analysis
+
+
+## Question 1: Is there a pulse
+## Response variable of interest: w mean (model weight, from 0 to 1)
+## Predictor variables of interest: varGroup, varType, Sample.unit, unitDuration, elev.m.wc, MAT.C.wc, MAP.mm.wc, Pulse.type
+
+
+## Question 2: If there is a pulse, do t.peak (time to peak response) 
+## and y.peak (magnitude of peak response) depend on the variables of interest?
+## Response of interest: y.peak and t.peak mean
+## Predictor variables of interest: varGroup, varType, Sample.unit, unitDuration, elev.m.wc, MAT.C.wc, MAP.mm.wc, Pulse.type
+
+
+## Question 3: If there is no pulse response, do bb (intercept) and mm (slope) depend on the variables of interest?
+## Response of interest: bb and mm mean
+## Predictor variables of interest: varGroup, varType, Sample.unit, unitDuration, elev.m.wc, MAT.C.wc, MAP.mm.wc, Pulse.type
+
+
+
+
